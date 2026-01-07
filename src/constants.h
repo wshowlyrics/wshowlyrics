11ffa8aa52385bdececf419648aba28c0531c03d
unstable-code <assa0620@gmail.com>
2026-01-07 23:30:07 +0900

    chore: Bump version to 0.9.3

--- src/constants.h
+++ src/constants.h
@@ -35,7 +35,7 @@
 
 // Network buffers
 #define URL_BUFFER_SIZE 2048
-#define USER_AGENT_STRING "wshowlyrics/0.9.2"
+#define USER_AGENT_STRING "wshowlyrics/0.9.3"
 
 // Cryptographic buffers
 #define MD5_DIGEST_STRING_LENGTH 33  // 32 hex chars + null terminator
